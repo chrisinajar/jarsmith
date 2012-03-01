3c8a8fb4a8afeb9c7e5ea4cac2b3aaed4619e6c1
Chris Vickery <chrisinajar@gmail.com>
2012-02-29 22:32:17 -0500

    Fixed globalEval

--- src/nquery/nquery.js
+++ src/nquery/nquery.js
@@ -4,14 +4,21 @@ var jQuery = require('./jquery.js'),
 // So these are my implementations, replace these to wrap it yourself! yay!
     Document = require('./document.js'),
     Navigator = require('./navigator.js'),
-    Location = require('./location.js');
+    Location = require('./location.js'),
+    Window = require('./window.js');
 
 var nQuery = function() {
   var document = new Document(this),
       navigator = new Navigator(this),
-      location = new Location(this);
+      location = new Location(this),
+      window = new Window(this);
   
-  this.jQuery = jQuery({document, navigator, location});
+  this.jQuery = jQuery({
+    window: window,
+    document: document,
+    navigator: navigator,
+    location: location,
+  });
   this.$ = this.jQuery;
 };
 
---
+++ src/nquery/window.js
@@ -0,0 +1,6 @@
+var Window = function() {
+};
+
+Window.prototype.eval = eval;
+
+module.exports = Window;
