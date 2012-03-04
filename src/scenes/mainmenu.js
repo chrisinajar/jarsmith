bcec3f8495dfc248a028969a3ccf827ba617bd8f
Chris Vickery <chrisinajar@gmail.com>
2012-03-03 19:48:50 -0500

    Made menu example much better

--- src/scenes/mainmenu.js
+++ src/scenes/mainmenu.js
@@ -1,10 +1,18 @@
 var resources = require('../resources'),
-    nQuery = require('../nquery/nquery');
+    nQuery = require('../nquery/nquery'),
+    keyboard = require('../keyboard');
 
 var $;
-var MainMenu = function(engine) {
+var MainMenu = function(engine, events) {
   this.engine = engine;
   $ = (new nQuery(engine)).$;
+  this.selected = null;
+  this.selectedIndex = 0;
+  this.menu = [
+    [370, 360],
+    [440, 360],
+    [640, 360],
+  ];
   engine.loadResources('menu', function(s) {
     var engine = s.engine;
     $('<obj>',  {
@@ -29,8 +37,61 @@ var MainMenu = function(engine) {
       .animate({left: 369.5}, 300, "swing")
       .animate({left: 349.5}, 300, "swing")
       .animate({left: 329.5, top: 90, width: 365, height: 184}, 100, "swing");
+  
+    s.selected = $('<obj>',  {
+      width: 300,
+      height: 120,
+      top: 370,
+      left: 360,
+      texture: 'selected',
+    });
   }, this);
+  
+  var self = this;
+  events.on('KEYDOWN', function(evt){self.keydown.apply(self, [evt]);});
+  events.on('KEYUP', function(evt){self.keyup.apply(self, [evt]);});
+};
 
+MainMenu.prototype.keydown = function (evt) {
+  switch (evt.sym) {
+    case keyboard.up:
+      this.moveUp();
+      break;
+    case keyboard.down:
+      this.moveDown();
+      break;
+  }
+};
+
+MainMenu.prototype.keyup = function (evt) {
+  var ascii = evt.sym;
+  
+  if( ( ascii < 123 ) && ( ascii > 96 ) ) {
+    if( 0 != ( evt.mod && 0x2003 ) ) {
+      ascii -= 32;
+    }
+  }
+  
+  console.log('keyup');
+  console.log(ascii);
+};
+
+MainMenu.prototype.moveUp = function() {
+  if (!this.selected)
+    return;
+  if (this.selectedIndex == 0)
+    this.selectedIndex = this.menu.length;
+  this.selectedIndex--;
+  this.selected.stop().animate({top: this.menu[this.selectedIndex][0], left: this.menu[this.selectedIndex][1]}, 200);
+};
+
+MainMenu.prototype.moveDown = function() {
+  if (!this.selected)
+    return;
+  this.selectedIndex++;
+  if (this.selectedIndex == this.menu.length)
+    this.selectedIndex = 0;
+  this.selected.stop().animate({top: this.menu[this.selectedIndex][0], left: this.menu[this.selectedIndex][1]}, 200);
 };
 
 module.exports = MainMenu;
